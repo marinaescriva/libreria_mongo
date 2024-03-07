ac7277929f3d04f0e31e59c6fd7bcd876fc9460a
marinaescriva <marinaescriva.24@gamil.com>
2024-03-07 18:49:04 +0100

    feature: book model created

---
+++ src/models/Books.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+
+const BookSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+       author: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+
+const Book = model('Book', BookSchema)
+
+export default Book
