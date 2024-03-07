5dcb090bd8d1bf71a1c60974d192dcc9f370f11b
marinaescriva <marinaescriva.24@gamil.com>
2024-03-07 21:00:15 +0100

    feature: add book delete endpoint

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -63,3 +63,92 @@ export const getBooks = async(req, res) =>{
         
     }
 }
+
+export const updateBookById = async(req, res) =>{
+   
+    try {
+        const { title } = req.body // el dato que queremos cambiar por body
+    
+        const bookId = req.params.id //como el isbn es único
+    
+        if (!title) {
+          return res.status(400).json(
+            {
+              success: true,
+              message: "title required",
+            }
+          )
+        }
+    
+        const bookUpdated = await Book.findOneAndUpdate( //busqueda de un libro por su title
+          {
+            _id: bookId 
+          },
+          {
+            title: title
+          },
+          {
+            new: true //muestra el update
+          }
+        )
+    
+        res.status(200).json(
+          {
+            success: true,
+            message: "Book updated",
+            data: bookUpdated
+          }
+        )
+      } catch (error) {
+        res.status(500).json(
+          {
+            success: false,
+            message: "Book cant retrieved",
+            error: error.message
+          }
+        )
+      }
+}
+
+export const deleteBookById = async (req, res) => {
+    
+    try {
+        const bookId = req.params.id // el dato que queremos eliminar
+    
+        if (!bookId) {
+          return res.status(400).json(
+            {
+              success: true,
+              message: "invalid bookId or isnt correct",
+            }
+          )
+        }
+        console.log(bookId , "1")
+        
+        const bookDeleted = await Book.findOneAndDelete(
+            {
+            _id:bookId  //busqueda de un libro y lo elimina si el id coincide con el que pasas en ruta
+            }
+        )
+
+        console.log(bookDeleted._id , "2")
+    
+        res.status(200).json(
+          {
+            success: true,
+            message: "Book deleted",
+            data: bookDeleted
+          }
+        )
+
+        console.log("3")
+      } catch (error) {
+        res.status(500).json(
+          {
+            success: false,
+            message: "Book cant be deleted",
+            error: error.message
+          }
+        )
+      }
+}
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,10 +1,12 @@
 import { Router} from "express";
-import { createBook, getBooks } from "../controllers/book.controller.js";
+import { createBook, deleteBookById, getBooks, updateBookById } from "../controllers/book.controller.js";
 
 
 const router = Router();
 
 router.post('/', createBook );
 router.get ('/', getBooks );
+router.put('/:id', updateBookById);
+router.delete('/:id', deleteBookById);
 
 export default router; 
